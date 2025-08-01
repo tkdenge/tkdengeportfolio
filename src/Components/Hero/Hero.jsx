e65effecb47c6d9e77c7aa4d10b15098cae9be94
Denge <tkdenge3@gmail.com>
2025-08-01 21:53:18 +0200

    Updated redirects directory again x3

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
           <div className="hero-connect"><AnchorLink className='anchor-link' href="#contact">Contact me!</AnchorLink></div>
           <div className="hero-resume">
             <a 
-              href="/public/CV TK.pdf" 
+              href="/CV_TK.pdf" 
               target="_blank" 
               rel="noopener noreferrer"
             >Resume</a>
