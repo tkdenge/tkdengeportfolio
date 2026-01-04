e5116a4718118c439d94d7c298d5e68868ee5832
Denge <tkdenge3@gmail.com>
2026-01-04 17:18:28 +0200

    Updated Profile_Img

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Hero.css'
-import profile_img from '../../assets/profileImage.jpg'
+import profile_img from '../../assets/PP.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
